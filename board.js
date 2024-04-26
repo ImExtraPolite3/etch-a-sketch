fb6763b291f02e186337cd3c15d440815431ac9c
ExtraPolite <imextrapolite2@gmail.com>
2024-04-26 18:18:05 -0400

    change background color after hover of mouse

--- board.js
+++ board.js
@@ -4,7 +4,6 @@ const board = function() {
   for (row = 0; row < 16; row++) {
     const allRows = document.createElement('div');
     allRows.className = 'all-rows';
-
     container.appendChild(allRows);
   }
 
@@ -14,10 +13,20 @@ const board = function() {
     for (let i = 0; i < 16; i++) {
       const square = document.createElement('div');
       square.className = 'square';
-  
       eachRow.appendChild(square);  
     }
   });
 }
 
+const addHoverEffect = function() {
+  const allSquares = document.querySelectorAll('.square');
+
+  allSquares.forEach(eachSquare => {
+    eachSquare.addEventListener('mouseover', () => {
+      eachSquare.className = 'square change-color';
+    })
+  });
+}
+
 board();
+addHoverEffect();
